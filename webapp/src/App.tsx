e731f7063c23496146d1fab6e532649edf81882d
Ayuya1013 <dorachann1013@gmail.om>
2025-10-30 14:16:17 +0900

    一応動いた．あとは図形位置調整．テキストボックスは作り直しカモ

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import arrow1 from "./arrow-1.svg";
-import ellipse1 from "./ellipse-1.svg";
-import icon from "./icon.svg";
-import polygon1 from "./polygon-1.svg";
-import rectangle1 from "./rectangle-1.svg";
-import sotowakuN from "./sotowaku-n.svg";
-import sotowakuP from "./sotowaku-p.svg";
-import star1 from "./star-1.svg";
+import arrow1 from "../public/login/Arrow-1.svg";
+import ellipse1 from "../public/login/Ellipse-1.svg";
+import icon from "../public/login/arrow_forward.svg";
+import polygon1 from "../public/login/Polygon-1.svg";
+import rectangle1 from "../public/login/Rectangle-1.svg";
+import sotowakuN from "../public/login/sotowaku_n.svg";
+import sotowakuP from "../public/login/sotowaku_p.svg";
+import star1 from "../public/login/Star-1.svg";
 
 export const Login = (): JSX.Element => {
   const [name, setName] = useState("");
@@ -18,7 +18,7 @@ export const Login = (): JSX.Element => {
   };
 
   return (
-    <div className="bg-purple w-full min-h-screen relative overflow-hidden">
+    <div className="bg-[var(--purple)] w-full min-h-screen relative overflow-hidden">
       {/* Decorative Images */}
       <img
         className="absolute right-7 bottom-[-30px] w-[346px] h-[300px]"
@@ -54,21 +54,24 @@ export const Login = (): JSX.Element => {
       {/* Login Form */}
       <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center absolute top-[448px] left-1/2 transform -translate-x-1/2 space-y-6">
         {/* Name Field */}
-        <div className="relative w-64">
-          <img className="w-full h-16 pointer-events-none" alt="名前入力枠" src={sotowakuN} />
-          <label htmlFor="name-input" className="absolute top-1 left-4 text-black font-bold text-sm">
-            名前
-          </label>
+        <div className="relative w-[300px] h-16 flex items-center justify-between px-4">
           <input
             type="text"
             id="name-input"
             name="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className="absolute top-6 left-4 w-[126px] text-gray bg-transparent border-none outline-none"
+            className="w-[220px] h-full bg-[#D8C9FF] rounded-xl border border-black text-gray px-4 outline-none"
             aria-label="名前"
             required
           />
+          <label
+            htmlFor="name-input"
+            className="text-black font-bold text-sm leading-tight tracking-widest text-right"
+            style={{ writingMode: "vertical-rl", textOrientation: "upright" }}
+          >
+            名前
+          </label>
         </div>
 
         {/* Password Field */}
@@ -105,3 +108,4 @@ export const Login = (): JSX.Element => {
     </div>
   );
 };
+export default Login;
